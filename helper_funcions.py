6eb63cf10441b150702d6da504705ffff2ecf853
kaezar22 <106036350+kaezar22@users.noreply.github.com>
2024-04-07 09:37:11 -0500

    Add files via upload

--- helper_funcions.py
+++ helper_funcions.py
@@ -286,3 +286,41 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
+
+
+# lets create a function to compare training histories
+def compare_historys(original_history, new_history, initial_epochs = 5):
+  """
+  compares two Tensorflow history objects
+  """
+  # get original history measurements
+  acc = original_history.history["accuracy"]
+  loss = original_history.history["loss"]
+
+  val_acc = original_history.history["val_accuracy"]
+  val_loss = original_history.history["val_loss"]
+
+  # Combien original history metrics with new history metrics
+  total_acc = acc + new_history.history["accuracy"]
+  total_loss = loss + new_history.history["loss"]
+
+  total_val_acc = val_acc + original_history.history["val_accuracy"]
+  total_val_loss = val_loss + original_history.history["val_loss"]
+
+  # MAke plots for accuracy
+  plt.figure( figsize = (8,8))
+  plt.subplot(2,1,1)
+  plt.plot(total_acc, label = "Training Accuracy")
+  plt.plot(total_val_acc, label = "Validation Accuracy")
+  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label = "Start Fine Tuning")
+  plt.legend(loc = "lower right")
+  plt.title("Training and validation accuracy")
+
+# MAke plots for accuracy
+  plt.figure( figsize = (8,8))
+  plt.subplot(2,1,2)
+  plt.plot(total_loss, label = "Training Loss")
+  plt.plot(total_val_loss, label = "Validation Loss")
+  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label = "Start Fine Tuning")
+  plt.legend(loc = "upper right")
+  plt.title("Training and validation Loss")
